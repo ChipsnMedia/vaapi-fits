706f7341d9972522b35df56c99fb5c34d693ec50
Gregory Yoon <gregory.yoon@outlook.com>
2021-11-05 10:56:42 +0900

    added mpeg2/mpeg2/h263/vp8/vc1 conformance files

---
+++ test/ffmpeg-vaapi/decode/h263.py
@@ -0,0 +1,25 @@
+###
+### Copyright (C) 2018-2019 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.ffmpeg.vaapi.util import *
+from ....lib.ffmpeg.vaapi.decoder import DecoderTest
+
+spec = load_test_spec("h263", "decode")
+
+@slash.requires(*platform.have_caps("decode", "h263"))
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "h263")
+    super(default, self).before()
+
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    self.case = case
+    self.decode()
